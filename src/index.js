4269be69ca582ee65cb3621880222dbbe3f54423
gawayumi <ayumifabih@gmail.com>
2025-02-14 19:25:32 -0300

    Projeto Finalizado

--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
-/*
-**O Que deve ser utilizado**
+const prompt = require('prompt-sync')();
 
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-
-## Objetivo
-
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-Se XP for menor do que 1.000 = Ferro
-Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
-Se XP for entre 5.001 e 7.000 = Ouro
-Se XP for entre 7.001 e 8.000 = Platina
-Se XP for entre 8.001 e 9.000 = Ascendente
-Se XP for entre 9.001 e 10.000= Imortal
-Se XP for maior ou igual a 10.001 = Radiante
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói de nome **{nome}** está no nível de **{nivel}**"
-*/
-
-var nome = prompt("Insira o nome do herói: ");
-
-var xp = prompt("Insira o nível de experiência do herói: ");
+const nome = prompt("Insira o nome do herói: ");
+const xpHeroi = prompt("Insira o nível de experiência do herói: ");
+const xp = Number(xpHeroi);
 
 function verificarNivel(xp) {
+    let nivel;
     if (xp < 1000) {
         nivel = "Ferro";
-    }
-    else if (xp > 1000 && xp < 2000) {
+    } else if (xp > 1000 && xp <= 2000) {
         nivel = "Bronze";
+    } else if (xp > 2000 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp > 5000 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp > 8000 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp > 9000 && xp <= 1000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
     }
+    return nivel;
 }
 
-console.log("O herói de nome " + nome + "está no nível " + nivel);
+let nivel = verificarNivel(xp); // passar o valor de xp ao chamar a função
+
+console.log("O herói de nome " + nome + " está no nível " + nivel);
